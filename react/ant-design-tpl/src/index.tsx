f949eaf190a8489fbf550335fe19ac9cbfe6b255
赵辉 <zhaohui03@qianxin.com>
2021-02-22 15:27:20 +0800

    chore: add antdesign project

---
+++ react/ant-design-tpl/src/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: zhaohui
+ * @Date: 2021-02-07 14:47:36
+ * @LastEditTime: 2021-02-22 15:04:05
+ * @LastEditors: zhaohui
+ * @Description: 
+ * @FilePath: /own-project/src/index.tsx
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'assets/styles/base.less'
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
